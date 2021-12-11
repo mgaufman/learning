c0c9c27dabc39145edb7c4ab6fd2eed0f2041a34
Maxim Gaufman <mgaufman@microsoft.com>
2021-12-11 19:51:58 +0300

    queue structure added & switched from float to decimal

--- Course1/Lesson2/Calc.ps1
+++ Course1/Lesson2/Calc.ps1
@@ -11,7 +11,7 @@ function fInitializeStack {
     )
     $StackInstance = @{
         "Data" = @($null)*$Length;
-        "Pointer" = [int]0}
+        "IndexOfNext" = [int]0}
     return $StackInstance
 }
 
@@ -20,11 +20,11 @@ function fPushToStack {
         [Parameter(Mandatory=$true)] [object]$StackInstance,
         [Parameter(Mandatory=$true)] $Value
     )
-    if ($StackInstance["Pointer"] -eq $StackInstance["Data"].Count) {
+    if ($StackInstance["IndexOfNext"] -eq $StackInstance["Data"].Count) {
         throw ("Stack overflow (an attempt was made to push a value to a full stack)")
     }
-    $StackInstance["Data"][$StackInstance["Pointer"]] = $Value
-    $StackInstance["Pointer"]++
+    $StackInstance["Data"][$StackInstance["IndexOfNext"]] = $Value
+    $StackInstance["IndexOfNext"]++
     return $Value
 }
 
@@ -32,11 +32,58 @@ function fPopFromStack {
     param(
         [Parameter(Mandatory=$true)] [object]$StackInstance
     )
-    if ($StackInstance["Pointer"] -eq 0) {
+    if ($StackInstance["IndexOfNext"] -eq 0) {
         throw ("An attempt was made to pop a value from an empty stack")
     }
-    $StackInstance["Pointer"]--
-    $Value = $StackInstance["Data"][$StackInstance["Pointer"]]
+    $StackInstance["IndexOfNext"]--
+    $Value = $StackInstance["Data"][$StackInstance["IndexOfNext"]]
+    return $Value
+}
+
+#***************************************
+
+function fInitializeQueue {
+    param (
+        [Parameter(Mandatory=$true)] [int]$Length
+    )
+    $QueueInstance = @{
+        "Data" = @($null)*$Length;
+        "IndexOfFirst"  = [int]0;
+        "CurrentLength" = [int]0
+    }
+    return $QueueInstance
+}
+
+function fPushToQueue {
+    param(
+        [Parameter(Mandatory=$true)] [object]$QueueInstance,
+        [Parameter(Mandatory=$true)] $Value
+    )
+    if ($QueueInstance["CurrentLength"] -eq $QueueInstance["Data"].Count) {
+        throw ("An attempt was made to push a value to a full queue")
+    }
+    [int]$IndexOfNext = $QueueInstance["IndexOfFirst"] + $QueueInstance["CurrentLength"]
+    if ($IndexOfNext -ge $QueueInstance["Data"].Count) {
+        $IndexOfNext = $IndexOfNext - $QueueInstance["Data"].Count
+    }
+    $QueueInstance["Data"][$IndexOfNext] = $Value
+    $QueueInstance["CurrentLength"]++
+    return $Value
+}
+
+function fPullFromQueue {
+    param(
+        [Parameter(Mandatory=$true)] [object]$QueueInstance
+    )
+    if ($QueueInstance["CurrentLength"] -eq 0) {
+        throw ("An attempt was made to pull a value from an empty queue")
+    }
+    $Value = $QueueInstance["Data"][$QueueInstance["IndexOfFirst"]]
+    $QueueInstance["IndexOfFirst"]++
+    if ($QueueInstance["IndexOfFirst"] -ge $QueueInstance["Data"].Count) {
+        $QueueInstance["IndexOfFirst"] = $QueueInstance["IndexOfFirst"] - $QueueInstance["Data"].Count
+    }
+    $QueueInstance["CurrentLength"]--
     return $Value
 }
 
@@ -54,7 +101,7 @@ $Operators = @{
 function fCalculateOperator {
     param (
         [Parameter(Mandatory=$true)] [string]$Operator,
-        [Parameter(Mandatory=$true)] [float[]]$Operands
+        [Parameter(Mandatory=$true)] [decimal[]]$Operands
     )
     switch ($Operator) {
         "#+" {$Result = $Operands[0]}
@@ -94,15 +141,18 @@ function fCalculateRpnExpression {
     $Stack = fInitializeStack -Length $StackLength
     $Tokens = $RpnExpr.Split(" ")
     for ($i = 0; $i -lt $Tokens.Count; $i++) {
+        if ($Tokens[$i].Length -eq 0) {     # Skipping mutiple spaces in the input string
+            continue
+        }
         if ($Operators.Keys -contains $Tokens[$i]) {
-            $Arguments = [float[]]@(0)*$Operators[$Tokens[$i]]
+            $Arguments = [decimal[]]@(0)*$Operators[$Tokens[$i]]
             for ($ArgNumber = $Operators[$Tokens[$i]]-1; $ArgNumber -ge 0; $ArgNumber--) {
-                $Arguments[$ArgNumber] = [float](fPopFromStack -StackInstance $Stack)
+                $Arguments[$ArgNumber] = [decimal](fPopFromStack -StackInstance $Stack)
             }
-            $Dummy = fPushToStack -StackInstance $Stack -Value (fCalculateOperator -Operator $Tokens[$i] -Operands $Arguments) # $Dummy prevents the function output to be passed through as return (PowerShell specifics)
+            fPushToStack -StackInstance $Stack -Value (fCalculateOperator -Operator $Tokens[$i] -Operands $Arguments) | Out-Null     # Out-Null prevents the function output to be passed through as return (PowerShell specifics)
         }
         else {
-            $Dummy = fPushToStack -StackInstance $Stack -Value $Tokens[$i]                                                 # $Dummy prevents the function output to be passed through as return (PowerShell specifics)
+            fPushToStack -StackInstance $Stack -Value $Tokens[$i] | Out-Null     # Out-Null prevents the function output to be passed through as return (PowerShell specifics)
         }
     }
     return (fPopFromStack -StackInstance $Stack)
